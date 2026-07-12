0a34d041abac377a48e20daab7aa1fde50612641
shivamsandy <shivamsandy3183@example.com>
2026-07-13 04:10:16 +0530

    add swap

---
+++ _swap.java
@@ -0,0 +1,9 @@
+public class _swap {
+    public static void main(String[] args) {
+      
+        String s  =  new StringBuilder("shivam").reverse().toString();
+        
+        System.err.println(s);
+    }
+    
+}
