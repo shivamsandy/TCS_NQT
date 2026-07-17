6605d186e4fab51c705ba3fccbef2645e4e53114
shivamsandy <shivamsandy3183@gmail.com>
2026-07-18 01:05:50 +0530

    daily commit

---
+++ _0032.java
@@ -0,0 +1,19 @@
+public class _0032 {
+    public static void main(String[] args) {
+        int[]  arr  =  {12,3,4,5,76,8,98,76,54,43346,56,76,8,5666,7678,897565};
+
+
+        int target  =   4 ; 
+        for(int i  =0 ;i<arr.length;i++){
+            if(arr[i]== target){
+                System.err.println("target HIT");
+            }
+        }
+
+
+
+
+        
+    }
+    
+}
